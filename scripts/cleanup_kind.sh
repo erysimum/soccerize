a3c8635f734f60a27c5dda1e2e5bccaaa3506fac
erysimum <cheirierysimum@gmail.com>
2025-06-11 13:22:14 +1000

    cleanup kind cluster

--- scripts/cleanup_kind.sh
+++ scripts/cleanup_kind.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-echo " Deleting KIND cluster: soccerize-kind-cluster"
-kind delete cluster --name soccerize-kind-cluster
+echo " Deleting KIND cluster: soccerize-cicd-cluster"
+kind delete cluster --name soccerize-cicd-cluster
 
 echo " Cluster deleted. Verifying..."
 kubectl config get-clusters
