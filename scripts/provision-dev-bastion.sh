576b462edd1979ac8b7c8d32d97dab7215b2917c
erysimum <cheirierysimum@gmail.com>
2025-06-24 17:16:03 +1000

    test script to generate eks cluster & nodegroup config

--- scripts/provision-dev-bastion.sh
+++ scripts/provision-dev-bastion.sh
@@ -13,12 +13,35 @@ terraform apply -auto-approve
 
 echo " Infra provisioned! Fetching outputs..."
 
-# Capture output
-BASTION_IP=$(terraform output -raw bastion_public_ip)
 
+
+# Capture output
+export vpc_id=$(terraform output -raw vpc_id)
+export subnet_a=$(terraform output -json private_subnets | jq -r '.[0]')
+export subnet_b=$(terraform output -json private_subnets | jq -r '.[1]')
+export bastion_ip=$(terraform output -raw bastion_public_ip)
+export ssh_key_name=$(terraform output -raw key_pair_name)
+#BASTION_IP=$(terraform output -raw bastion_public_ip)
+
+cd ../../eks-cluster
+echo " Generating cluster-config.yml..."
+envsubst < cluster-config.tpl.yml > cluster-config.yml
+
+echo " Generating nodegroup-config.yml..."
+envsubst < nodegroup-config.tpl.yml > nodegroup-config.yml
+
+
+
+echo " cluster-config.yml is generated..."
+cat cluster-config.yml
+echo " VPC ID: $vpc_id"
+echo " Subnet A: $subnet_a"
+echo " Subnet B: $subnet_b"
+echo " Key name":$ssh_key_name
+echo " Bastion IP=>: $bastion_ip"
 echo ""
 echo ""
-echo " Bastion EC2 Public IP==>>: $BASTION_IP"
+
 echo " Connect to our Bastion via SSH into it:"
-echo "ssh -i ~/projects/soccerize/terra-key ubuntu@$BASTION_IP"
+echo "ssh -i ~/projects/soccerize/terra-key ubuntu@$bastion_ip"
 
